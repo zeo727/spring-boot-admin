e5e568f39985a04e82724b01b0d6f699f832e8f0
likexin <likexin>
2020-04-01 14:36:52 +0800

    spring boot admin实现spring boot actuator监控可视化

---
+++ src/test/java/com/itmuch/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.springbootadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
